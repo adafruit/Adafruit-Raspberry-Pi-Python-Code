c00dc3c9a8a900a2e8220a89213b1e7b75eb24c0
K. Townsend <kevin@adafruit.com>
2012-10-26 19:51:10 +0200

    Updated example code

--- Adafruit_MCP230xx/Adafruit_MCP230xx.py
+++ Adafruit_MCP230xx/Adafruit_MCP230xx.py
@@ -100,8 +100,7 @@ class Adafruit_MCP230XX(object):
         return self.direction
 
     def output(self, pin, value):
-        assert self.direction & (1 << pin) == 0, "Pin %s not set to output" % pin
-
+        # assert self.direction & (1 << pin) == 0, "Pin %s not set to output" % pin
         if self.num_gpios <= 8:
             self.outputvalue = self._readandchangepin(MCP23008_GPIO, pin, value. self.i2c.readU8(MCP23008_OLAT))
         if self.num_gpios <= 16:
@@ -153,30 +152,22 @@ class MCP230XX_GPIO(object):
         
 
 if __name__ == '__main__':
-
-    # input test
-#    for i in range(16):
-#        mcp.pullup(i, 1)
-#    while (True):
-#        for i in range(16):
-#           print "%d: %x" % (i, mcp.input(i) >> i)
-
-    # output test
-#    for i in range(16):
-#        mcp.config(i, OUTPUT)
-#    while (True):
-#        for i in range(16):
-#            mcp.output(i, 0)
-#        time.sleep(0.1)
-#        for i in range(16):
-#            mcp.output(i, 1)
-#        time.sleep(0.1)
-
-
-    mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
-    mcp.config(8, OUTPUT)
-    mcp.config(7, OUTPUT)
-    mcp.config(6, OUTPUT)
-    mcp.output(8, 1)
-    mcp.output(7, 1)
-    mcp.output(6, 1)
+    # Use busnum = 0 for older Raspberry Pi's (pre 512MB)
+    mcp = Adafruit_MCP230XX(busnum = 0, address = 0x20, num_gpios = 16)
+    # Use busnum = 1 for new Raspberry Pi's (512MB)
+    # mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
+	
+	# Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
+    mcp.config(0, OUTPUT)
+    mcp.config(1, OUTPUT)
+    mcp.config(2, OUTPUT)
+	
+	# Set pin 3 to input with the pullup resistor enabled
+    mcp.pullup(3, 1)
+    # Read pin 3 and display the results
+    print "%d: %x" % (3, mcp.input(3) >> 3)
+	
+    # Python speed test on output 0 toggling at max speed
+    while (True):
+      mcp.output(0, 1)  # Pin 0 High
+      mcp.output(0, 0)  # Pin 1 Low
