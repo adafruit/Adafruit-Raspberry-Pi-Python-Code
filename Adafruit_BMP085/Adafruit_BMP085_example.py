457b4d0ff8e0f928b95fa6d48e77e81aa97752cb
KTOWN <kevin@adafruit.com>
2013-08-19 18:46:40 +0200

    More accurate altitude calculations

--- Adafruit_BMP085/Adafruit_BMP085_example.py
+++ Adafruit_BMP085/Adafruit_BMP085_example.py
@@ -17,8 +17,18 @@ bmp = BMP085(0x77)
 # bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
 
 temp = bmp.readTemperature()
+
+# Read the current barometric pressure level
 pressure = bmp.readPressure()
+
+# To calculate altitude based on an estimated mean sea level pressure
+# (1013.25 hPa) call the function as follows, but this won't be very accurate
 altitude = bmp.readAltitude()
+
+# To specify a more accurate altitude, enter the correct mean sea level
+# pressure level.  For example, if the current pressure level is 1023.50 hPa
+# enter 102350 since we include two decimal places in the integer value
+# altitude = bmp.readAltitude(102350)
 
 print "Temperature: %.2f C" % temp
 print "Pressure:    %.2f hPa" % (pressure / 100.0)
